55db377bff75be4d4934097f94e5c0493583f1db
Shraddha singh <shraddha09singh06@gmail.com>
2026-03-16 11:04:04 +0530

    Refactor database URI configuration
    Update SQLAlchemy database URI configuration to use environment variable and support PostgreSQL.

--- app.py
+++ app.py
@@ -10,7 +10,12 @@ from model.users import User
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key_here'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://rootlocalhost:3306/yo'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://rootlocalhost:3306/yo'
+db_url = os.environ.get("DATABASE_URL")
+
+if db_url and db_url.startswith("postgres://"):
+    db_url = db_url.replace("postgres://", "postgresql://", 1)
+app.config["SQLALCHEMY_DATABASE_URI"] = db_url
 
 loginmanager = LoginManager()   
 loginmanager.init_app(app)
